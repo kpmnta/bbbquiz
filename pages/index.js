4f5d15d710b9ed5061d3ea9eee9159872c840088
kai <kai.pimenta1@gmail.com>
2021-01-27 19:41:22 -0300

    state e components

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,55 @@
-import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizContainer from '../src/components/QuizContainer';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName ] = React.useState('');
   return (
+    <>
+    <Head>
+      <title>BBB Quiz</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <QuizBackground backgroundImage={ db.bg }>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>O quanto você manja de BBB?</h1>
           </Widget.Header>  
           <Widget.Content>
-            <p>fabidao diauisdhas fiuhauish sdhbais</p>
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              router.push(`/quiz?name${name}`)
+              console.log('fazendo submissão via react')
+              }} 
+            >
+              <Input 
+                name="name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                placeholder="me diz seu nome :)" 
+              />
+              <Button 
+                type="submit" 
+                disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Content>
-            <h1>O quanto você manja de BBB?</h1>
+            <h1>Quizes da Galera</h1>
             <p>fabidao diauisdhas fiuhauish sdhbais</p>
           </Widget.Content>
         </Widget>
@@ -38,5 +57,6 @@ export default function Home() {
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/kpmnta" />
     </QuizBackground>
+    </>
   );
 }
