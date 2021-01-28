76450f62e0fcb04d0c7e962a66f912e58924d893
kai <kai.pimenta1@gmail.com>
2021-01-28 12:10:50 -0300

    estilo component form alternativas

---
+++ src/components/AlternativesForm/index.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const AlternativesForm = styled.form `
+    label{
+        &[data-selected="true"] {
+            background-color: ${({ theme }) => theme.colors.primary};
+
+            &[data-selected="SUCESS"] {
+                background-color: ${({ theme }) => theme.colors.sucess};   
+            }
+
+            &[data-selected="ERROR"] {
+                background-color: ${({ theme }) => theme.colors.wrong};
+            }
+        }
+
+        &:focus {
+            opacity: 1;
+        }
+
+    }
+
+    button {
+        margin-top: 24px;
+    }
+
+`;
+
+export default AlternativesForm;
